ffb777b5b9cf5cd066b2e56e04a74327540eb15c
Tashira Vishan <tashira.pvt01@gmail.com>
2026-09-16 01:31:42 +0530

    update bubble sort.py

--- bubble sort.py
+++ bubble sort.py
@@ -2,12 +2,12 @@
 
 list_1 = [5, 2, 1, 8]
 
-count = 0
+n = len(list_1)
 
-for i in list_1:
-    if list_1[count] > list_1[count + 1]:
-        list_1[count + 1] = list_1[count]
-    count = count + 1
+for i in range(n - 1):
+    for j in range(n - i - 1):
+        if list_1[j] > list_1[j + 1]:
+            list_1[j], list_1[j + 1] = list_1[j + 1], list_1[j]
 
 
 print(list_1)
